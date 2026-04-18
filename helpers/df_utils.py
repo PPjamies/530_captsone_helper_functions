f7feefde493139ba9323015ee90c9bd353457034
ppjamies <pj.jindrich@gmail.com>
2026-04-17 21:27:36 -0700

    update cast_df_to_schema() to create column if it doesn't exist.

--- helpers/df_utils.py
+++ helpers/df_utils.py
@@ -9,15 +9,19 @@ def tag_source_type(df: DataFrame, source_type: SourceType, column_name: str = "
     """
     Tags a DataFrame with a constant value to indicate the data origin
     """
-    res = df.withColumn(column_name, lit(source_type.value))
-    return res
+    return df.withColumn(column_name, lit(source_type.value))
 
 
 def cast_df_to_schema(df: DataFrame, schema: StructType) -> DataFrame:
     """
     Forces a DataFrame to match a specific schema by casting columns
     """
-    res = df.select([
-        col(field.name).cast(field.dataType) for field in schema
+
+    existing_columns = set(df.columns)
+
+    return df.select([
+        col(field.name).cast(field.dataType)
+        if field.name in existing_columns
+        else lit(None).cast(field.dataType).alias(field.name)
+        for field in schema
     ])
-    return res
